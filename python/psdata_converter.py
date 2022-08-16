40c6621782ca8689aa97cf68c3067d7e9a60b498
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2022-08-16 16:53:58 -0700

    Converter can now make Matlab files directly at destination instead of moving them later

--- python/psdata_converter.py
+++ python/psdata_converter.py
@@ -2,10 +2,8 @@ from pathlib import Path
 import asyncio
 from typing import Optional, TypeVar, List, Iterable
 from itertools import islice
-from shutil import move
 from math import ceil
 import timeit
-from aiofiles.os import wrap
 import sys
 
 MAX_CONCURRENT_TASKS = 5
@@ -19,8 +17,15 @@ def subprocess_results_printer(command_text: str, returncode: int, stdout: Optio
     if stderr:
         print(f'[stderr]\n{stderr}')
         
-def generate_shell_command(file_path: Path):
-    return ['picoscope', '/c', f'{file_path.resolve()}', '/f', f'{FORMAT}', '/b', 'all']
+def generate_shell_command(file_path: Path, destination: Path):
+    return [
+        'picoscope', 
+        '/c', f'{file_path.resolve()}', 
+        '/d', f'{destination.resolve()}',
+        '/f', f'{FORMAT}', 
+        '/q',
+        '/b', 'all'
+        ]
 
 def get_limited_files(folder_path: Path, limit: Optional[int]):
     if limit is None:
@@ -38,20 +43,20 @@ def make_chunks(list_to_chunk: List[T], chunk_size: int) -> Iterable[List[T]]:
 def get_number_of_chunks(list_length: int, chunk_size: int) -> int:
     return ceil(list_length / chunk_size)
 
-async def process_file_async(file_path: Path):
-    proc = await asyncio.create_subprocess_exec(*generate_shell_command(file_path), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, encoding='utf-8')
-    stdout, stderr = await proc.communicate()
+async def process_file_async(file_path: Path, destination: Path):
+    proc = await asyncio.create_subprocess_exec(*generate_shell_command(file_path, destination))
+    await proc.communicate()
     if proc.returncode is not None:
         returncode = proc.returncode
     else:  # only happens if process not done, which should never happen with await
         returncode = 420  # computer must be high
     subprocess_results_printer(f'Converting {file_path.name}', returncode, None, None)
     
-async def process_files_async(folder_path: Path, destination: Path):
+async def process_files_async(folder_path: Path, destination: Path, num_files = FILES_LIMIT):
     start = timeit.default_timer()
-    movefile = wrap(move)
+    # movefile = wrap(move)
     
-    cors = [process_file_async(file_path) for file_path in get_limited_files(folder_path, FILES_LIMIT)]
+    cors = [process_file_async(file_path, destination) for file_path in get_limited_files(folder_path, num_files)]
     # Chunking code from https://fredrikaverpil.github.io/2017/06/20/async-and-await-with-subprocesses/
     if MAX_CONCURRENT_TASKS == 0:
         chunks = [cors]
@@ -64,8 +69,12 @@ async def process_files_async(folder_path: Path, destination: Path):
         await asyncio.gather(*chunk)
         print(f'Completed work on chunk {chunk_index+1}/{num_chunks}')
     
-    files_cors = [movefile(str(x), destination) for x in folder_path.iterdir() if x.is_dir()]
-    await asyncio.gather(*files_cors)
+    # files_cors = [movefile(str(x), destination) for x in folder_path.iterdir() if x.is_dir()]
+    # await asyncio.gather(*files_cors)
+
+    stop = timeit.default_timer()
+    exec_time = stop - start
+    print(f"Method executed in {exec_time:.4f} seconds")
 
     stop = timeit.default_timer()
     exec_time = stop - start
