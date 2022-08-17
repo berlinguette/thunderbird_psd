9b76a6aa1e6f44085ec475e1dc1d290925524445
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2022-08-17 14:56:27 -0700

    Code reformatting

--- python/psdata_converter.py
+++ python/psdata_converter.py
@@ -27,15 +27,17 @@ def subprocess_results_printer(
     if stderr:
         logger.debug(f'[stderr]\n{stderr}')
 
+
 def generate_shell_command(file_path: Path, destination: Path):
     return [
-        'picoscope', 
-        '/c', f'{file_path.resolve()}', 
+        'picoscope',
+        '/c', f'{file_path.resolve()}',
         '/d', f'{destination.resolve()}',
-        '/f', f'{FORMAT}', 
+        '/f', f'{FORMAT}',
         '/q',
         '/b', 'all'
-        ]
+    ]
+
 
 def get_limited_files(folder_path: Path, limit: Optional[int]):
     if limit is None:
@@ -44,11 +46,15 @@ def get_limited_files(folder_path: Path, limit: Optional[int]):
         files = islice(folder_path.iterdir(), limit)
     return files
 
+
 T = TypeVar('T')
+
+
 def make_chunks(list_to_chunk: List[T], chunk_size: int) -> Iterable[List[T]]:
     # Taken from https://stackoverflow.com/a/312464
     for i in range(0, len(list_to_chunk), chunk_size):
-        yield list_to_chunk[i : i + chunk_size]
+        yield list_to_chunk[i: i + chunk_size]
+
 
 def get_number_of_chunks(list_length: int, chunk_size: int) -> int:
     return ceil(list_length / chunk_size)
@@ -65,19 +71,27 @@ def chunkify(list_to_chunk: List[T], chunk_size: int) -> Tuple[Iterable[List[T]]
 
 
 async def process_file_async(file_path: Path, destination: Path):
-    proc = await asyncio.create_subprocess_exec(*generate_shell_command(file_path, destination))
+    proc = await asyncio.create_subprocess_exec(
+        *generate_shell_command(file_path, destination))
     await proc.communicate()
     if proc.returncode is not None:
         returncode = proc.returncode
     else:  # only happens if process not done, which should never happen with await
         returncode = 420  # computer must be high
-    subprocess_results_printer(f'Converting {file_path.name}', returncode, None, None)
-    
-async def process_files_async(folder_path: Path, destination: Path, num_files = FILES_LIMIT):
+    subprocess_results_printer(
+        f'Converting {file_path.name}', returncode, None, None)
+
+
+async def process_files_async(
+    folder_path: Path,
+    destination: Path,
+    num_files=FILES_LIMIT,
+    chunk_size=MAX_CONCURRENT_TASKS
+):
     start = timeit.default_timer()
-    # movefile = wrap(move)
-    
-    cors = [process_file_async(file_path, destination) for file_path in get_limited_files(folder_path, num_files)]
+
+    cors = [process_file_async(file_path, destination)
+            for file_path in get_limited_files(folder_path, num_files)]
     # Chunking code from https://fredrikaverpil.github.io/2017/06/20/async-and-await-with-subprocesses/
     chunks, chunks_count = chunkify(cors, chunk_size)
     for chunk_index, chunk in enumerate(chunks):
@@ -89,7 +103,13 @@ async def process_files_async(folder_path: Path, destination: Path, num_files = 
     exec_time = stop - start
     logger.debug(f"Method executed in {exec_time:.4f} seconds")
 
-def process_files_popen(folder_path: Path, destination: Path, num_files=FILES_LIMIT):
+
+def process_files_popen(
+    folder_path: Path,
+    destination: Path,
+    num_files=FILES_LIMIT,
+    chunk_size=MAX_CONCURRENT_TASKS
+):
     start = timeit.default_timer()
 
     file_paths = [file_path for file_path in get_limited_files(
@@ -110,9 +130,16 @@ def process_files_popen(folder_path: Path, destination: Path, num_files=FILES_LI
     exec_time = stop - start
     logger.debug(f"Method executed in {exec_time:.4f} seconds")
 
-def convert_psdata_directory(folder_path: Path, destination: Path, num_files=FILES_LIMIT):
-    asyncio.run(process_files_async(folder_path, destination))
-    # process_files_popen(folder_path, destination, num_files)
+
+def convert_psdata_directory(
+    folder_path: Path,
+    destination: Path,
+    num_files=FILES_LIMIT,
+    chunk_size=MAX_CONCURRENT_TASKS
+):
+    # asyncio.run(process_files_async(folder_path, destination, num_files, chunk_size))
+    process_files_popen(folder_path, destination, num_files, chunk_size)
+
 
 if __name__ == "__main__":
     if 'win32' in sys.platform:
--- python/runner.py
+++ python/runner.py
@@ -8,20 +8,25 @@ import PySimpleGUI as sg
 from main import parquetize_directory
 from psdata_converter import MAX_CONCURRENT_TASKS, convert_psdata_directory
 
+MAX_CONCURRENT_TASKS = 5
 WINDOW_TITLE = 'Select Raw Data Folder'
 
 logger = logging.getLogger('main')
+
+
 def prepare_destination(destination_path: Path):
     if destination_path.is_dir():
         rmtree(destination_path)
     else:
         destination_path.mkdir()
 
+
 def select_folder() -> Optional[Path]:
-    left_col = [[sg.Text('Folder'), sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()]]
+    left_col = [[sg.Text('Folder'), sg.In(
+        size=(25, 1), enable_events=True, key='-FOLDER-'), sg.FolderBrowse()]]
     layout = [[sg.Column(left_col, element_justification='c')]]
-    window = sg.Window(WINDOW_TITLE, layout,resizable=True)
-    
+    window = sg.Window(WINDOW_TITLE, layout, resizable=True)
+
     done = False
     folder = None
     while not done:
@@ -30,21 +35,26 @@ def select_folder() -> Optional[Path]:
             done = True
             if event == '-FOLDER-':
                 folder = values['-FOLDER-']
-    
+
     window.close()
     return Path(folder)
 
-if __name__ == "__main__":
+
+def main():
     psdata_folder_path = None
     while psdata_folder_path is None:
         psdata_folder_path = select_folder()
+
     matlab_directory = psdata_folder_path.parent / 'mat'
     parquet_directory = psdata_folder_path.parent.parent / 'processed_data' / 'parquets'
     prepare_destination(matlab_directory)
     prepare_destination(parquet_directory)
+
     logger.info("Converting PSData to Matlab")
     convert_psdata_directory(psdata_folder_path, matlab_directory)
     logger.info("Converting Matlab to Parquet")
     parquetize_directory(matlab_directory, parquet_directory)
 
-    
+
+if __name__ == "__main__":
+    main()
