681e58a414197c7c3a6f3ebf5f4de833e81933f3
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2022-09-01 11:02:34 -0700

    Config and CL args now setup from one YAML file

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -1,7 +1,12 @@
+from __future__ import annotations
+
 from pathlib import Path
-from typing import Dict, Optional
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Union
 
 import yaml
+
+if TYPE_CHECKING:
+    from argparse import ArgumentParser
 
 
 def _override_config(old_config: Dict, new_config: Dict) -> Dict:
@@ -25,39 +30,169 @@ def _override_config(old_config: Dict, new_config: Dict) -> Dict:
     return {**old_config, **new_config}
 
 
-def _load_configuration(config_path: Optional[Path] = None) -> Dict:
-    """Loads the default configuration file, as well as any user-generated 
-       configuration files, producing one set of configuration data
-
-    Any items in the user-generated configuration file will override any 
-    matching items from the default configuration file. 
+def _load_yaml_dict_file(file_path: Path) -> Dict[str, Any]:
+    """Loads the given YAML file
 
     Parameters
     ----------
-    config_path : Optional[Path], optional
-        path to the user-generated configuration file. If not given, only the 
-        default configuration file is used
+    file_path : Path
+        path to YAML file to be loaded
 
     Returns
     -------
     Dict
-        Resulting configuration data
+        file data
     """
-    # this assumes default_config is in same dir as this py file
-    default_path = Path(__file__).parent / 'default_config.yaml'
-    with open(default_path, 'r') as conf_file:
-        config = yaml.safe_load(conf_file)
+    setup_file_path = Path(__file__).parent / 'config_fields_setup.yaml'
+    with open(setup_file_path, 'r') as setup_file:
+        file_data = yaml.safe_load(setup_file)
 
-    # given config overrides any default config values
-    if config_path is not None:
-        with open(config_path, 'r') as conf_file:
-            new_config = yaml.safe_load(conf_file)
-            config = _override_config(config, new_config)
+    if isinstance(file_data, dict) and all(isinstance(key, str) for key in file_data.keys()):
+        return file_data
+    else:
+        raise ValueError(
+            f"YAML file at {file_path} did not have dictionary data")
 
+
+def load_config_setup() -> Dict[str, Any]:
+    """Loads the configuration setup file
+
+    Returns
+    -------
+    Dict
+        configuration setup data
+    """
+    # assumes setup file is in same dir as this .py file
+    setup_file_path = Path(__file__).parent / 'config_fields_setup.yaml'
+    return _load_yaml_dict_file(setup_file_path)
+
+
+def _generate_default_config(config_setup: Dict[str, Any]) -> Dict:
+    """Generates default configuration data from the configuration setup
+
+    Args:
+        config_setup (Dict): _description_
+
+    Returns:
+        Dict: _description_
+    """
+    config = {k: v['config']['default'] for k, v in config_setup.items()
+              if 'config' in v.keys() and isinstance(v['config'], dict)}
     return config
 
 
-def get_configuration(cli_args: Dict, config_path: Optional[Path] = None) -> Dict:
+def _validate_config(config: Dict, config_setup: Dict[str, Any]) -> bool:
+    """Validates that config data is valid
+
+    Config data is valid if:
+    - config key exists in setup
+    - config value has correct type
+    - config value is >= 0 if it's an integer
+
+    Args:
+        config (Dict): user-generated config file data to be validated
+        config_setup (Dict): configuration setup data
+
+    Returns:
+        bool: True if config file data has valid types and values, False if not
+    """
+    ValueType = Union[None, bool, int, float, str]
+
+    def test_int_valid(value: ValueType) -> bool:
+        if isinstance(value, int):
+            return value >= 0
+        else:
+            return False
+
+    def is_valid_type(value: ValueType, type_string: str) -> bool:
+        tests: Dict[str, Callable[[ValueType], bool]] = {
+            'none': lambda x: x is None,
+            'bool': lambda x: isinstance(x, bool),
+            'int': test_int_valid,
+            'float': lambda x: isinstance(x, float),
+            'str': lambda x: isinstance(x, str)
+        }
+        type_test = tests[type_string]
+        return type_test(value)
+
+    def check_config_item(key: Any, value: Any) -> bool:
+        setup_data = config_setup.get(key)
+        if setup_data is None:
+            return False
+
+        allowed_types: List[str] = setup_data['config']['allowed_types']
+        return any([
+            is_valid_type(value, type_string) for type_string in allowed_types
+        ])
+
+    return all([check_config_item(k, v) for k, v in config.items()])
+
+
+def populate_args_parser(
+    parser: ArgumentParser,
+    config_setup: Dict[str, Any]
+) -> ArgumentParser:
+    """Add arguments to ArgumentParser based on configuration setup data
+
+    Configuration setup YAML file should have a 'cli' section for each setting,
+    with all needed data to add an argument. At minimum, this includes a list 
+    of flags under the 'flags' key, but can also include any other keyword 
+    argument from add_arguments().  (See the argparse documentation for more 
+    details.)
+
+    Parameters
+    ----------
+    parser : ArgumentParser
+        the parser to be populated with arguments
+    config_setup : Dict[str, Any]
+        configuration setup data
+
+    Returns
+    -------
+    ArgumentParser
+        populated parser
+
+    Raises
+    ------
+    ValueError
+        when configuration setup data does not have the correct format (i.e. 
+        missing 'cli' and 'flags' keys)
+    """
+    type_str_to_type: Dict[str, type] = {
+        'int': int,
+        'float': float,
+    }
+
+    setting_data: Dict[str, Any]
+    for setting, setting_data in config_setup.items():
+        try:
+            cli_data: Dict[str, Any] = setting_data['cli']
+            flags: Union[List[str], str] = cli_data['flags']
+            # YAML parse: single item list != list. Thanks YAML parse...
+            if not isinstance(flags, list):
+                flags = [flags]
+            kwargs = {k: v for k, v in cli_data.items() if k != 'flags'}
+            real_type: Optional[type] = type_str_to_type.get(
+                kwargs.get('type', 'unknown'), None)
+            if real_type is not None:
+                kwargs['type'] = real_type
+            parser.add_argument(*flags, **kwargs)
+        except KeyError as e:
+            raise ValueError(
+                "Config setup does not have correct format ",
+                f"for setting {setting}\n",
+                f"Config setup: {config_setup}\n",
+                f"Original exception:\n{e}"
+            )
+
+    return parser
+
+
+def get_configuration(
+    cli_args: Dict,
+    config_setup: Dict,
+    user_config_path: Optional[Path] = None
+) -> Dict:
     """Gets the complete configuration data from any config files and command
        line arguments
 
@@ -73,7 +208,9 @@ def get_configuration(cli_args: Dict, config_path: Optional[Path] = None) -> Dic
     ----------
     cli_args : Dict
         command line arguments, as {setting_name: value}
-    config_path : Optional[Path], optional
+    config_setup: Dict
+        configuration setup data
+    user_config_path : Optional[Path], optional
         path to the user-generated configuration file. If not given, only the 
         default configuration file is used
 
@@ -82,9 +219,36 @@ def get_configuration(cli_args: Dict, config_path: Optional[Path] = None) -> Dic
     Dict
         Configuration data, as {setting_name: value}
     """
-    config = _load_configuration(config_path)
+    default_config = _generate_default_config(config_setup)
+
+    config = default_config
+    if user_config_path is not None:
+        new_config = _load_yaml_dict_file(user_config_path)
+        if _validate_config(new_config, config_setup):
+            config = _override_config(config, new_config)
+
     # omitted args get None value, which would override config
     # so we must remove them
     cli_args = {k: v for k, v in cli_args.items() if v is not None}
     config = _override_config(config, cli_args)
+
     return config
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    config_setup = load_config_setup()
+    default_config = _generate_default_config(config_setup)
+    print(f"Default config:\n{default_config}")
+
+    new_config_data = {'files_limit': 10}
+    config_valid = _validate_config(new_config_data, config_setup)
+    print(f"config valid? {config_valid}")
+    if config_valid:
+        config = _override_config(default_config, new_config_data)
+        print(f"Merged config:\n{config}")
+
+    parser = populate_args_parser(ArgumentParser(), config_setup)
+    print("Parser help:")
+    parser.print_help()
