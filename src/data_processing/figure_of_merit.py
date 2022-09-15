46153bc712d2463c5f7b1d8578adf31355da05ae
A5H-git <alvn.hendricks@gmail.com>
2022-09-15 15:18:18 -0700

    Imported new functions, clean-up and commenting

--- src/data_processing/figure_of_merit.py
+++ src/data_processing/figure_of_merit.py
@@ -1,21 +1,17 @@
 import numpy as np
-from scipy import signal
+import pandas as pd
+
+from scipy import interpolate, signal
 from scipy.optimize import curve_fit
 
 
 def gaussian(x: list, mu: float, sigma: float, A: float) -> list:
     """Returns a Gaussian distribution"""
-    return A * np.exp(-((x - mu)**2 / (2 * (sigma**2))))
+    return A * np.exp(-((x - mu) ** 2 / (2 * (sigma**2))))
 
 
 def bimodal(
-    x: list,
-    mu1: float,
-    sigma1: float,
-    A1: float,
-    mu2: float,
-    sigma2: float,
-    A2: float
+    x: list, mu1: float, sigma1: float, A1: float, mu2: float, sigma2: float, A2: float
 ) -> list:
     """Returns a bimodal Gaussian distribution"""
     return gaussian(x, mu1, sigma1, A1) + gaussian(x, mu2, sigma2, A2)
@@ -31,9 +27,7 @@ def FOM(mu1: float, sigma1: float, mu2: float, sigma2: float) -> float:
 
 
 def guess_bimodal_params(
-    counts: np.ndarray,
-    bins: np.ndarray,
-    widths: list = [3, 5, 10, 20]
+    counts: np.ndarray, bins: np.ndarray, widths: list = [3, 5, 10, 20]
 ) -> float:
     """Returns a guess starting condition for :func:`scipy.optimize.curvefit`"""
     idxs = signal.find_peaks_cwt(counts, widths)  # May need adjusting?
@@ -46,14 +40,17 @@ def guess_bimodal_params(
 
     sigma1, sigma2 = 0.02, 0.02
 
-    return bins[:-1][pk_idx1], sigma1, counts[pk_idx1], bins[:-1][pk_idx2], sigma2, counts[pk_idx2]
+    return (
+        bins[:-1][pk_idx1],
+        sigma1,
+        counts[pk_idx1],
+        bins[:-1][pk_idx2],
+        sigma2,
+        counts[pk_idx2],
+    )
 
 
-def fit_fom(
-    counts: list,
-    bins: list,
-    guesses: tuple = None
-) -> tuple[tuple, float]:
+def fit_fom(counts: list, bins: list, guesses: tuple = None) -> tuple[tuple, float]:
     """Attempts to fit two Gaussians to the Figure of Merit"""
 
     if guesses is None:
@@ -67,3 +64,41 @@ def fit_fom(
     params, cov = curve_fit(bimodal, bins[:-1], counts, starting_guesses)
 
     return abs(params), cov
+
+
+def n_sigma_classifier(
+    psd: pd.DataFrame, gauss_params: tuple, n: float, n_bins: int = 100
+) -> tuple[pd.DataFrame, pd.DataFrame, interpolate.interp1d, interpolate.interp1d]:
+    """Classifies the neutrons given a value :param:`n` sigma above the mean + stdev
+    of the gamma distribution
+    """
+    _, bins = np.histogram(psd.loc["tail / total"], n_bins)
+
+    mu, sigma, A = gauss_params
+    gamma_gauss = gaussian(bins[:-1], mu, sigma, A)
+    gate_gauss = gaussian(bins[:-1], mu + n * sigma, sigma, A)
+
+    y_gate = gate_gauss[gate_gauss.argmax() :]
+    y_gate_bins = bins[:-1][gate_gauss.argmax() :]
+    y_gamma = gamma_gauss[gamma_gauss.argmax() :]
+    y_gamma_bins = bins[:-1][gamma_gauss.argmax() :]
+
+    f_gate = interpolate.interp1d(
+        y_gate_bins, y_gate, kind="linear", fill_value="extrapolate"
+    )
+    f_gamma = interpolate.interp1d(
+        y_gamma_bins, y_gamma, kind="linear", fill_value="extrapolate"
+    )
+
+    def neutron_filter(signal):
+        # amplitude and tail/total swapped because axis are swapped during graphing
+        gate_cond = signal["amplitude"] <= f_gate(signal["tail / total"])
+        gamma_cond = signal["amplitude"] > f_gamma(signal["tail / total"])
+        return gate_cond and gamma_cond
+
+    filt = psd.apply(neutron_filter)
+
+    neutrons = psd.T[filt].T
+    gammas = psd.T[~filt].T
+
+    return neutrons, gammas, f_gate, f_gamma
--- src/data_processing/psd.py
+++ src/data_processing/psd.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
+
+from scipy import interpolate
 from data_processing.peak_finding import get_bases
+from data_processing.figure_of_merit import gaussian
 
 
 def generate_psd(
@@ -7,11 +11,13 @@ def generate_psd(
     left_bases: dict,
     right_bases: dict,
     amplitudes: dict,
-    cutoff_voltage: float
+    cutoff_voltage: float,
 ) -> pd.DataFrame:
+    """Returns the Pulse Shape Discrimination report"""
+
     def process(series: pd.Series) -> dict:
-        q_total = series[left_bases[series.name]:].sum()
-        q_tail = series[right_bases[series.name]:].sum()
+        q_total = series[left_bases[series.name] :].sum()
+        q_tail = series[right_bases[series.name] :].sum()
 
         res = {
             "total integral": q_total,
@@ -22,7 +28,8 @@ def generate_psd(
         return res
 
     psd_report = pd.DataFrame(
-        {signal_id: process(df[signal_id]) for signal_id in df.columns})
+        {signal_id: process(df[signal_id]) for signal_id in df.columns}
+    )
     psd_report.loc["amplitude"] = amplitudes
 
     filt = psd_report.loc["tail / total"].between(-0.1, 0.8)
@@ -46,11 +53,9 @@ def df_to_psd(
     Args
     -----
     peak_offset: index shifted from the peak where its base may begin; used to calculate the peak's base value
-    tail_onset: index shifted from the peak where the tail should start 
+    tail_onset: index shifted from the peak where the tail should start
     """
     df = df - dc_offset
-    output = df.apply(lambda x: get_bases(
-        x, x.idxmax(), peak_offset, tail_onset)
-    )
+    output = df.apply(lambda x: get_bases(x, x.idxmax(), peak_offset, tail_onset))
     left_bases, right_bases = output.iloc[0, :], output.iloc[1, :]
     return generate_psd(df, left_bases, right_bases, df.max(), cutoff_voltage)
