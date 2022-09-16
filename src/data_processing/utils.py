2ffac7d526cad1296ba71d40b53c203ebfd9beac
A5H-git <alvn.hendricks@gmail.com>
2022-09-16 13:43:59 -0700

    Added utility functions

---
+++ src/data_processing/utils.py
@@ -0,0 +1,87 @@
+import datetime
+from pathlib import Path
+
+import pandas as pd
+import tomli
+import tomli_w
+
+from data_processing.processing_configs import (
+    MIN_SNR,
+    PK_FIND_HEIGHT,
+    PK_FIND_PROMINENCE,
+    TRIGGER_THRESHOLD,
+)
+
+
+def load_exp_info(exp_info_path: Path) -> dict:
+    # TODO: add checking
+    with open(exp_info_path, "rb") as f:
+        exp_info = tomli.load(f)
+
+    return exp_info
+
+
+def dump_settings(destination: Path) -> None:
+    multipeak_filter_settings = {
+        "height": PK_FIND_HEIGHT,
+        "prominence": PK_FIND_PROMINENCE,
+    }
+
+    incomplete_filter_settings = {"threshold": TRIGGER_THRESHOLD}
+
+    snr_filter_settings = {"threshold": MIN_SNR}
+
+    report = {
+        "last_updated": datetime.datetime.now(),
+        "multipeak_filter_settings": multipeak_filter_settings,
+        "incomplete_filter_settings": incomplete_filter_settings,
+        "snr_filter_settings": snr_filter_settings,
+    }
+
+    with open(destination, "w+b") as f:
+        tomli_w.dump(report, f)
+
+
+def generate_report(
+    num_initial_signals: int,
+    num_missing_signals: int,
+    num_multipeaks: int,
+    num_incomplete: int,
+    num_low_snr: int,
+    final_size: int,
+) -> dict:
+    report = {
+        "last_updated": datetime.datetime.now(),
+        "initial": num_initial_signals,
+        "missing_or_nan": num_missing_signals,
+        "multipeaks": num_multipeaks,
+        "incomplete": num_incomplete,
+        "low_snr": num_low_snr,
+        "final": final_size,
+    }
+    return report
+
+
+def save_parquet(df: pd.DataFrame, filename: str, destination: Path) -> None:
+    df = df.T
+    df.columns = df.columns.astype("str")
+    df.to_parquet(destination / filename)
+
+
+def save_report(uid: str, signal_stats: dict, destination: Path) -> None:
+    try:
+        with open(destination, "r+b") as f:
+            saved_report = tomli.load(f)
+
+            if uid in saved_report.keys():
+                saved_report[uid].update(signal_stats)
+            else:
+                saved_report[uid] = signal_stats
+
+            with open(destination, "wb") as f:
+                sorted_report = {key: saved_report[key] for key in sorted(saved_report)}
+                tomli_w.dump(sorted_report, f)
+
+    except FileNotFoundError:
+        with open(destination, "w+b") as f:
+            tomli_w.dump({uid: signal_stats}, f)
