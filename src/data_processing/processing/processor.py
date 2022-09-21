7a8cc8530795ac991a90436f3a295d2b1ee257cc
A5H-git <alvn.hendricks@gmail.com>
2022-09-21 15:53:58 -0700

    Updated to run in src and added logging

--- src/data_processing/processing/processor.py
+++ src/data_processing/processing/processor.py
@@ -1,37 +1,56 @@
+import logging
+import time
 from pathlib import Path
 from typing import Optional
 
 import numpy as np
 import pandas as pd
-from reporting.plot_configs import QUOTIENT_LOWER_LIM, QUOTIENT_UPPER_LIM
-from reporting.plotting import (
+from data_processing.processing.figure_of_merit import FOM, fit_fom, n_sigma_classifier
+from data_processing.processing.peak_finding import get_bases
+from data_processing.processing.processing_configs import *
+from data_processing.processing.psd import generate_psd
+from data_processing.reporting.plot_configs import (
+    QUOTIENT_LOWER_LIM,
+    QUOTIENT_UPPER_LIM,
+)
+from data_processing.reporting.plotting import (
     plot_bounded_scatter,
     plot_classification_with_grouping,
     plot_fom,
 )
-from reporting.reporting import save_plot
-
-from processing.figure_of_merit import FOM, fit_fom, n_sigma_classifier
-from processing.peak_finding import get_bases
-from processing.processing_configs import *
-from processing.psd import generate_psd
+from data_processing.reporting.reporting import save_plot
+from logging_helpers.setup_logger import (
+    cleanup_logger,
+    message_debug,
+    message_info,
+    setup_logger,
+)
 
 
 def process_file(
     df: pd.DataFrame,
     elapsed_time: float,
+    root_dir: Path,
     buffer_id: Optional[str] = None,
     plot_path: Optional[Path] = None,
 ):
+    logger = logging.getLogger(f"Data-Processing-{buffer_id}")
+    setup_logger(logger, root_dir.parent.parent)
+    t1 = time.perf_counter()
+
+    message_info("Removing fine DC offset", logger)
     df = df - FINE_DC_OFFSET
 
+    message_info("Getting bases", logger)
     output = df.apply(
         lambda series: get_bases(series, series.idxmax(), PEAK_OFFSET, TAIL_ONSET)
     )
     left_bases, right_bases = output.iloc[0, :], output.iloc[1, :]
 
+    message_info("Computing PSD metrics", logger)
     psd_report = generate_psd(df, left_bases, right_bases, df.max(), CUTOFF_VOLTAGE)
 
+    message_info("Fitting Figure of Merit", logger)
     counts, bins = np.histogram(psd_report.loc["tail / total"], N_BINS)
     params, _ = fit_fom(counts, bins)
     neutrons, gammas, f_gate, f_gamma = n_sigma_classifier(
@@ -39,6 +58,7 @@ def process_file(
     )
 
     if plot_path is not None:
+        message_info("Plotting graphs", logger)
         tail_vs_total, _ = plot_bounded_scatter(
             psd_report.loc["total integral"],
             psd_report.loc["tail integral"],
@@ -73,4 +93,5 @@ def process_file(
             plot_path / buffer_id, classification, f"{buffer_id}-classification.png"
         )
 
+    message_debug(f"Elapsed Time: {time.perf_counter() - t1}", logger, on_screen=False)
     return FOM(*params[0:2], *params[3:-1]), neutrons.shape[1] / elapsed_time
--- src/data_processing/processor.py
+++ src/data_processing/processor.py
@@ -1,52 +1,68 @@
+import logging
 import re
 import time
 from pathlib import Path
 
 import pandas as pd
+from logging_helpers.setup_logger import (
+    cleanup_logger,
+    message_debug,
+    message_info,
+    setup_logger,
+)
 
-from cleaning.cleaner import clean_file
-from processing.processor import process_file
-from saving.io import dump_settings, save_parquet, save_report
+from data_processing.cleaning.cleaner import clean_file
+from data_processing.processing.processor import process_file
+from data_processing.saving.io import dump_settings, save_parquet, save_report
 
-if __name__ == "__main__":
-    ROOT_DIR = Path("../../sample_datasets/20220906_AmBe/")
-    PARQ_PATH = ROOT_DIR / "raw_data/parquet/20220906-0005.parquet"
-    EXP_TIMES_PATH = ROOT_DIR / "raw_data/exp_times.csv"
-    # PLOT_PATH = ROOT_DIR / "processed_data/plots"
-    PLOT_PATH = None
-    DESTINATION = ROOT_DIR / "processed_data/cleaned_buffers"
+
+def main(directory: Path):
+    PARQ_PATH = directory / "raw_data/parquet/20220906-0005.parquet"
+    EXP_TIMES_PATH = directory / "raw_data/exp_times.csv"
+    PLOT_PATH = directory / "processed_data/plots"
+    # PLOT_PATH = None
+    DESTINATION = directory / "processed_data/cleaned_buffers"
     SETTINGS_PATH = DESTINATION / "settings.toml"
     REPORT_PATH = DESTINATION / "report.toml"
 
+    uid = PARQ_PATH.name.split(".")[0]
+    logger = logging.getLogger(f"Data-Pipeline-{uid}")
+    setup_logger(logger, directory.parent.parent)
     t1 = time.perf_counter()
 
-    df_clean, report, _ = clean_file(PARQ_PATH, ROOT_DIR, PLOT_PATH)
+    df_clean, report, _ = clean_file(PARQ_PATH, directory, PLOT_PATH)
 
+    message_info("Dumping settings", logger)
     dump_settings(
-        ROOT_DIR / "settings.toml" if SETTINGS_PATH is None else SETTINGS_PATH
+        directory / "settings.toml" if SETTINGS_PATH is None else SETTINGS_PATH
     )
 
-    uid = PARQ_PATH.name.split(".")[0]
-
+    message_info("Saving processing report", logger)
     save_report(
-        uid, report, ROOT_DIR / "report.toml" if REPORT_PATH is None else REPORT_PATH
+        uid, report, directory / "report.toml" if REPORT_PATH is None else REPORT_PATH
     )
 
+    message_info("Saving processed parquet file", logger)
     filename = re.sub("(\d+\-\d+)(.parquet)", r"\1_clean\2", PARQ_PATH.name)
     save_parquet(
         df_clean,
         filename,
-        ROOT_DIR if DESTINATION is None else DESTINATION,
+        directory if DESTINATION is None else DESTINATION,
     )
 
+    message_info("Computing values", logger)
     buffer_number = int(re.split("-(\d+)[_.]", PARQ_PATH.name)[1])
     elapsed_time = pd.read_csv(EXP_TIMES_PATH, index_col=0).at[
         buffer_number, "elapsed_s"
     ]
 
-    fom, cps = process_file(
-        df_clean, elapsed_time, PARQ_PATH.name.split(".")[0], PLOT_PATH
-    )
-    print(f"FOM={fom}; CPS={cps}")
+    fom, cps = process_file(df_clean, elapsed_time, ROOT_DIR, PLOT_PATH)
+    print(f"FOM={fom}; CPS={cps}")  # TODO: Update this
 
-    print(f"Compelted in: {time.perf_counter() - t1}")
+    message_debug(f"Elapsed Time: {time.perf_counter() - t1}", logger, on_screen=False)
+    cleanup_logger(logger)
+
+
+if __name__ == "__main__":
+    ROOT_DIR = Path("../sample_datasets/20220906_AmBe/")
+    main(ROOT_DIR)
