09fa47e8db933ab74c21728c39f7d268c36713d8
A5H-git <alvn.hendricks@gmail.com>
2022-09-22 12:13:10 -0700

    Added documentation

--- src/data_processing/processor.py
+++ src/data_processing/processor.py
@@ -5,11 +5,7 @@ from pathlib import Path
 from typing import Optional
 
 import pandas as pd
-from logging_helpers.setup_logger import (
-    cleanup_logger,
-    message_info,
-    setup_logger,
-)
+from logging_helpers.setup_logger import cleanup_logger, message_info, setup_logger
 
 from data_processing.cleaning.cleaner import clean_file
 from data_processing.processing.processor import process
@@ -19,8 +15,34 @@ from data_processing.saving.io import dump_settings, save_parquet, save_report
 def process_file(
     parquet_path: Path,
     plot_destination: Optional[Path] = None,
-    config_destination: Optional[Path] = None,
+    report_destination: Optional[Path] = None,
 ) -> None:
+    """Executes data processing pipeline and generates several pieces of data.
+    - Report of data cleaning statistics
+    - (TODO:) Report of data processing statistics
+    - Settings used during processing
+    - Plots:
+        - Cleaned signals plot
+        - Neutron/Gamma Classification
+        - Figure of Merit (FOM)
+        - tail vs total integrals
+        - tail/total integrals (q) vs amplitude
+    - A parquet file post data cleaning
+
+    **NOTE:** This method assumes that `ROOT_DIR` is located in a directory that follows the parquet file
+    is stored **3** folders into the experiment root; e.g. `./root/raw_data/parquet/my.parquet`
+
+    Parameters
+    ----------
+    parquet_path : Path
+        The path to the parquet file
+    plot_destination : Optional[Path]
+        The location where plots will be stored. If `plot_destination` is not specified, no plots
+        will be generated
+    report_destination : Optional[Path]
+        The location where reports will be stored. If not specified, data will be stored in a `processed_data`
+        folder inside of root
+    """
     ROOT_DIR = parquet_path.parent.parent.parent
     EXP_TIMES_PATH = ROOT_DIR / "raw_data/exp_times.csv"
     PARQ_DESTINATION = ROOT_DIR / "processed_data/cleaned_buffers"
@@ -37,8 +59,8 @@ def process_file(
     message_info("Dumping settings", logger)
     dump_settings(
         PARQ_DESTINATION / "settings.toml"
-        if config_destination is None
-        else config_destination
+        if report_destination is None
+        else report_destination
     )
 
     message_info("Saving processing report", logger)
@@ -46,8 +68,8 @@ def process_file(
         uid,
         report,
         PARQ_DESTINATION / "report.toml"
-        if config_destination is None
-        else config_destination,
+        if report_destination is None
+        else report_destination,
     )
 
     message_info("Saving processed parquet file", logger)
@@ -80,9 +102,29 @@ def process_directory(
     plot_destination: Optional[Path] = None,
     config_destination: Optional[Path] = None,
 ) -> None:
+    """Executes data processing pipeline on raw parquets in a directory.
+    See `process_file()` for output information.
+
+    Parameters
+    ----------
+    directory : Path
+        The path to the directory that contains the parquet files
+    n_start : Optional[int]
+        The starting index of parquets to use; default set to 1 since `exp_times.csv`
+        does record correct elapsed time
+    n_end : Optional[int]
+        The number of parquets to process. if not specified, the entire directory
+        will be processed starting from `n_start`
+    plot_destination : Optional[Path]
+        The location where plots will be stored. If `plot_destination` is not specified, no plots
+        will be generated
+    report_destination : Optional[Path]
+        The location where reports will be stored. If not specified, data will be stored in a `processed_data`
+        folder inside of root
+    """
     PARQ_PATHS = [f for f in directory.iterdir()]
 
-    for PARQ_PATH in PARQ_PATHS[n_start : n_end + 1]:
+    for PARQ_PATH in PARQ_PATHS[n_start : n_end + 1 if n_end is not None else n_end]:
         process_file(PARQ_PATH, plot_destination, config_destination)
 
 
