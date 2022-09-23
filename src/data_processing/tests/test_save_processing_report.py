8b3ff47bbb18153fd13180e3e5d42038e7de6196
A5H-git <alvn.hendricks@gmail.com>
2022-09-23 11:52:13 -0700

    Added test for save_results

---
+++ src/data_processing/tests/test_save_processing_report.py
@@ -0,0 +1,65 @@
+import os
+from pathlib import Path
+from tkinter.tix import COLUMN
+
+import pandas as pd
+from data_processing.saving.io import save_results
+
+ROOT = Path("./data_processing/tests")
+
+COLUMN_LABELS = ["counts", "counts / sec", "FOM"]
+
+
+def test_save_new_results():
+    # Arrange: Prepare a new dataframe to be saved
+    destination = ROOT / "io_test_files/report_new.csv"
+
+    df = pd.DataFrame([(420, 512, 690)], columns=COLUMN_LABELS, index=["20220906-0002"])
+
+    # Act: Save results
+    save_results(df, destination)
+
+    # Assert: Check if df is correctly saved
+    new_df = pd.read_csv(destination, index_col=0)
+    assert new_df.equals(df)
+
+    # Post: Reset directory so that test can be rerun
+    os.remove(destination)
+
+
+def test_modify_results():
+    # Arrange: Prepare a new dataframe to saved
+    unchanged_row_vals = 122.0, 155.0, 184.0
+    unchanged_row_label = "20220906-0115"
+
+    df_original = pd.DataFrame(
+        [(420.0, 512.0, 690.0), (unchanged_row_vals)],
+        columns=COLUMN_LABELS,
+        index=["20220906-0001", unchanged_row_label],
+    )
+
+    destination = ROOT / "io_test_files/report_stock.csv"
+    save_results(df_original, destination)
+
+    # Update existing and add new entry
+    df_new = pd.DataFrame(
+        [(112.0, 612.0, 332.0), (176.0, 591.0, 132.0)],
+        columns=COLUMN_LABELS,
+        index=["20220906-0001", "20220906-0420"],
+    )
+
+    # Act: Save results and prepare for comparison
+    save_results(df_new, destination)
+
+    df_modified = pd.read_csv(destination, index_col=0)
+    df_unchanged_row = pd.DataFrame(
+        [(unchanged_row_vals)], columns=COLUMN_LABELS, index=[unchanged_row_label]
+    )
+    df_comparator = pd.concat([df_new, df_unchanged_row])
+    df_comparator = df_comparator.sort_index()
+
+    # Assert: Check if df is correctly modified with new values
+    assert df_modified.equals(df_comparator)
+
+    # Post: Reset directory so that test can be rerun
+    os.remove(destination)
