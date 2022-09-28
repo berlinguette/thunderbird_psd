1c9234914b42027ff010ff1eabb80b30392089f7
Thunderbird <tblabpc@berlinguettegroup.com>
2022-09-28 13:35:36 -0700

    Ran Black and ported to new logging system

--- src/data_processing/cleaning/cleaner.py
+++ src/data_processing/cleaning/cleaner.py
@@ -7,15 +7,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from data_processing.cleaning.cleaning_configs import *
-from data_processing.cleaning.data_cleaning import (filter_incomplete_triggers,
-                                                    filter_low_snr,
-                                                    filter_multipeaks, rms,
-                                                    subtract_rms)
+from data_processing.cleaning.data_cleaning import (
+    filter_incomplete_triggers,
+    filter_low_snr,
+    filter_multipeaks,
+    rms,
+    subtract_rms,
+)
 from data_processing.reporting.plotting import plot_signal
 from data_processing.reporting.reporting import generate_report, save_plot
 from data_processing.saving.io import load_exp_info
-from logging_helpers.setup_logger import (message_debug, message_info,
-                                          setup_logger)
+from logging_helpers.setup_logger import Messenger, setup_logger
 
 
 def clean_file(
@@ -50,27 +52,28 @@ def clean_file(
     """
     file_name = filepath.name.split(".")[0]
     logger = logging.getLogger(f"Data-Cleaning-{file_name}")
+    messenger = Messenger(logger)
     setup_logger(logger, root_dir.parent.parent / "processing.log")
     t1 = time.perf_counter()
 
-    message_info("Reading parquet file", logger)
+    messenger.info("Reading parquet file", logger)
     df = pd.read_parquet(filepath)
     df.columns = df.columns.astype("int16")
     num_initial_signals = df.shape[0]
 
-    message_info("Dropping invalid data points", logger)
+    messenger.info("Dropping invalid data points", logger)
     df = df.replace([np.inf, -np.inf], np.nan).dropna(how="any")
     num_missing_signals = num_initial_signals - df.shape[0]
 
     df = df.T
 
-    message_info("Removing baseline offset", logger)
+    messenger.info("Removing baseline offset")
     dc_offset = df.iloc[0:50].mean()
     df_offset = -1 * (df - dc_offset)
     baseline_rms = df_offset.apply(lambda x: rms(x, RMS_CUTOFF))
     df_offset = df_offset.apply(lambda x: subtract_rms(x, baseline_rms))
 
-    message_info("Filtering multipeaks", logger)
+    messenger.info("Filtering multipeaks")
     df_single_peaks, props = filter_multipeaks(
         df_offset, height=PK_FIND_HEIGHT, prominence=PK_FIND_PROMINENCE
     )
@@ -81,7 +84,7 @@ def clean_file(
     )
     num_incomplete = df_single_peaks.shape[0] - df_complete_triggers.shape[0]
 
-    message_info("Filtering low signal-to-noise", logger)
+    messenger.info("Filtering low signal-to-noise")
     peak_heights = props.apply(lambda x: x["peak_heights"][0])
     df_high_snr = filter_low_snr(
         df_complete_triggers, peak_heights, baseline_rms, MIN_SNR
@@ -89,19 +92,19 @@ def clean_file(
     num_low_snr = df_complete_triggers.shape[0] - df_high_snr.shape[0]
 
     if plot_destination is not None:
-        message_info("Plotting clean signals", logger)
+        messenger.info("Plotting clean signals")
         try:
             exp_info_path = root_dir / "exp_info.toml"
             exp_info = load_exp_info(exp_info_path)
             sample_interval = exp_info["picoscope"]["sample_interval"]
         except FileNotFoundError:
             sample_interval = 2  # Assume it's 2 ns
-        
+
         fig, _ = plot_signal(df_complete_triggers, sample_interval)
         save_plot(plot_destination / file_name, fig, f"{file_name}-cleaned_signals.png")
         plt.close(fig)
 
-    message_info("Generating cleaning report", logger)
+    messenger.info("Generating cleaning report")
     report = generate_report(
         num_initial_signals,
         num_missing_signals,
@@ -111,7 +114,5 @@ def clean_file(
         df_high_snr.shape[0],
     )
 
-    message_debug(
-        f"Elapsed Time: {time.perf_counter() - t1:.3f} s", logger, on_screen=False
-    )
+    messenger.debug(f"Elapsed Time: {time.perf_counter() - t1:.3f} s", on_screen=False)
     return df_high_snr, report, props
