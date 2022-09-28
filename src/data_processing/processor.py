a485e4763898371e403dfa73fc9a5e7f6ea340d7
Thunderbird <tblabpc@berlinguettegroup.com>
2022-09-28 13:32:30 -0700

    Reformat and port to new logging system

--- src/data_processing/processor.py
+++ src/data_processing/processor.py
@@ -5,16 +5,16 @@ from pathlib import Path
 from typing import Optional
 
 import pandas as pd
-from logging_helpers.setup_logger import cleanup_logger, message_info, setup_logger
+from logging_helpers.setup_logger import Messenger, cleanup_logger, setup_logger
 
 from data_processing.cleaning.cleaner import clean_file
 from data_processing.processing.processor import process
 from data_processing.saving.io import (
+    create_folder,
     dump_settings,
     save_cleaning_report,
     save_parquet,
     save_results,
-    create_folder
 )
 
 
@@ -61,22 +61,22 @@ def process_file(
     PARQ_DESTINATION = ROOT_DIR / "processed_data/cleaned_buffers"
 
     uid = parquet_path.name.split(".")[0]
-
     logger = logging.getLogger(f"Data-Pipeline-{uid}")
+    messenger = Messenger(logger)
     setup_logger(logger, PARQ_DESTINATION.parent / "processing.log")
-    message_info(f"--- Starting processing for buffer {uid} ---", logger)
+    messenger.info(f"--- Starting processing for buffer {uid} ---")
     t1 = time.perf_counter()
 
     df_clean, report, _ = clean_file(parquet_path, ROOT_DIR, plot_destination)
 
-    message_info("Dumping settings", logger)
+    messenger.info("Dumping settings")
     dump_settings(
         PARQ_DESTINATION / "cleaning_settings.toml"
         if report_destination is None
         else report_destination
     )
 
-    message_info("Saving processing report", logger)
+    messenger.info("Saving processing report")
     save_cleaning_report(
         uid,
         report,
@@ -85,11 +85,11 @@ def process_file(
         else report_destination,
     )
 
-    message_info("Saving processed parquet file", logger)
+    messenger.info("Saving processed parquet file")
     filename = re.sub("(\d+\-\d+)(.parquet)", r"\1_clean\2", parquet_path.name)
     save_parquet(df_clean, filename, PARQ_DESTINATION)
 
-    message_info("Computing values", logger)
+    messenger.info("Computing values")
     buffer_number = int(re.split("-(\d+)[_.]", parquet_path.name)[1])
     elapsed_time = pd.read_csv(EXP_TIMES_PATH, index_col=0).at[
         buffer_number, "elapsed_s"
@@ -100,11 +100,12 @@ def process_file(
 
     processing_time = time.perf_counter() - t1
 
-    message_info(
-        f"Processed {uid} showing: Count={counts} neutrons | CPS={cps:.3f} neutrons/sec | FOM={fom:.3f}", logger)
+    messenger.info(
+        f"Processed {uid} showing: Count={counts} neutrons | CPS={cps:.3f} neutrons/sec | FOM={fom:.3f}"
+    )
 
-    message_info(
-        f"--- Completed processing buffer {uid} in {processing_time:.3f} s ---", logger
+    messenger.info(
+        f"--- Completed processing buffer {uid} in {processing_time:.3f} s ---"
     )
 
     return fom, counts, cps
@@ -143,24 +144,23 @@ def process_directory(
         folder inside of root
     """
     logger = logging.getLogger("Directory-Processor")
+    messenger = Messenger(logger)
     setup_logger(logger, directory.parent.parent.parent / "processor.log")
 
     PARQ_PATHS = [f for f in directory.iterdir()]
     n_end = n_end + 1 if n_end is not None else -1
-
 
     create_folder(directory.parent.parent / "processed_data/cleaned_buffers")
 
     if plot_destination is not None:
         create_folder(plot_destination)
 
-    message_info("Intiating directory processing...", logger)
+    messenger.info("Intiating directory processing...")
     column_labels = ["neutron_count", "count_rate [s^-1]", "FOM"]
     results = pd.DataFrame(columns=column_labels)
     for PARQ_PATH in PARQ_PATHS[n_start:n_end]:
-        message_info(f"Processing {PARQ_PATH.name}")
-        fom, counts, cps = process_file(
-            PARQ_PATH, plot_destination, config_destination)
+        messenger.info(f"Processing {PARQ_PATH.name}")
+        fom, counts, cps = process_file(PARQ_PATH, plot_destination, config_destination)
         df = pd.DataFrame(
             [(int(counts), round(cps, 3), round(fom, 3))],
             columns=column_labels,
@@ -168,32 +168,28 @@ def process_directory(
         )
         results = pd.concat([results, df])
 
-    save_results(results, directory.parent.parent /
-                 "processed_data/report.csv")
+    save_results(results, directory.parent.parent / "processed_data/report.csv")
 
-    message_info("Completed directory processing!", logger)
+    messenger.info("Completed directory processing!")
     cleanup_logger(logger)
 
 
 if __name__ == "__main__":
 
-    BASE_PATH = Path("C:/Users/Neutron Computer/Documents/Data/20220922_AmBedistancetests/")
+    BASE_PATH = Path(
+        "C:/Users/Neutron Computer/Documents/Data/20220922_AmBedistancetests/"
+    )
     exlcusions = ["AmBe_0cm_1525V", "AmBe_30cm_1525V"]
     for ROOT_DIR in BASE_PATH.iterdir():
 
         if not ROOT_DIR.is_dir() or ROOT_DIR.name not in ["AmBe_20cm_1525V"]:
             continue
 
-        
         PLOT_PATH = ROOT_DIR / "processed_data/plots"
 
-
         # 0 and 30 cm bad...
-        process_directory(
-            ROOT_DIR / "raw_data/parquet", plot_destination=PLOT_PATH
-        )
+        process_directory(ROOT_DIR / "raw_data/parquet", plot_destination=PLOT_PATH)
 
     # ROOT_DIR = Path("C:/Users/Neutron Computer/Documents/Data/20220922_AmBedistancetests/AmBe_100cm_1525V")
     # PARQ_PATH = ROOT_DIR / "raw_data/parquet/20220906-0005.parquet"
     # process_file(PARQ_PATH, PLOT_PATH, None)
-
