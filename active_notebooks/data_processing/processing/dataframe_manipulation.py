d3f38b84cd6a54290780bb8363155c9f266d8242
Ryan Oldford <ryan.oldford@gmail.com>
2023-04-03 16:36:49 -0700

    Neutron signal data can now be queried and plotted

---
+++ active_notebooks/data_processing/processing/dataframe_manipulation.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+def generate_full_neutron_df(classified_df: pd.DataFrame,
+                             signals_df: pd.DataFrame,
+                             classification_col: str = "NASA"):
+    neutrons_only = classified_df.query(classification_col)
+    full_neutron_df = neutrons_only.join(signals_df)
+    return full_neutron_df
+
+
+def generate_neutron_signals(
+    neutron_events_df: pd.DataFrame,
+    signal_length: int = 200
+) -> pd.DataFrame:
+    signal_start = '0'
+    signal_end = str(signal_length-1)
+    neutron_signals = neutron_events_df.loc[:, signal_start:signal_end]
+    neutron_signals = neutron_signals.astype(int)
+    neutron_signals = neutron_signals.transpose()
+    return neutron_signals
+
+
+# def select_neutron_traces(
+#     neutron_events_df: pd.DataFrame,
+#     query_params: dict[str, tuple[float | None, float | None]]
+# ) -> pd.DataFrame:
+#     psd_start, psd_end = query_params.get('PSD', (None, None))
+#     eng_start, eng_end = query_params.get('Energy', (None, None))
+#     time_start, time_end = query_params.get('Time', (None, None))
+    
+#     query_cols = ["`tail / total`", 'CALIB_ENERGY', 'EVENT_TIME']
+#     query_var_name = ['psd', 'eng', 'time']
+#     start_vars = [psd_start, eng_start, time_start]
+#     end_vars = [psd_end, eng_end, time_end]
+    
+#     start_queries = [f"{col} >= @{var_name}_start" 
+#                      for col, var_name, start_var
+#                      in zip(query_cols, query_var_name, start_vars)
+#                      if start_var is not None]
+#     end_queries = [f"{col} >= @{var_name}_start" 
+#                      for col, var_name, end_var
+#                      in zip(query_cols, query_var_name, end_vars)
+#                      if end_var is not None]
+#     queries = start_queries + end_queries
+    
+#     query_string = ' & '.join(queries)
+#     matching_neutrons = neutron_events_df.query(query_string)
+#     return matching_neutrons
---
+++ active_notebooks/data_processing/reporting/signal_investigator.py
@@ -0,0 +1,158 @@
+import pandas as pd
+from data_processing.processing.dataframe_manipulation import generate_neutron_signals
+from dataclasses import dataclass, field, fields
+from datetime import datetime
+from typing import Generic, TypeVar, Protocol, Any
+from abc import abstractmethod
+from dataframe_validation import PsdDfColumn
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+import matplotlib.pyplot as plt
+from data_processing.reporting.plot_configs import *
+from numpy import arange
+
+C = TypeVar("C", bound="Comparable")
+
+
+class Comparable(Protocol):
+    @abstractmethod
+    def __eq__(self, __value: Any) -> bool:
+        pass
+    
+    @abstractmethod
+    def __lt__(self: C, __value: C) -> bool:
+        pass
+    
+    def __gt__(self: C, __value: C) -> bool:
+        return (not self < __value) and self != __value
+    
+    def __le__(self: C, __value: C) -> bool:
+        return self < __value or self == __value
+    
+    def __ge__(self: C, __value: C) -> bool:
+        return (not self < __value)
+    
+
+class QueryRange(Generic[C]):
+    def __init__(self, col: PsdDfColumn) -> None:
+        self._start: C | None = None
+        self._end: C | None = None
+        self._col = col
+    
+    @property
+    def start(self) -> C | None:
+        return self._start
+    
+    @start.setter
+    def start(self, value: C | None):
+        if (value is not None and
+            self._start is not None and
+            self._end is not None and
+            value > self._end):
+            self._start, self._end = self._end, value
+        else:
+            self._start = value
+            
+    @property
+    def end(self) -> C | None:
+        return self._end
+    
+    @end.setter
+    def end(self, value: C | None):
+        if (value is not None and
+            self._start is not None and
+            self._end is not None and
+            value < self._start):
+            self._start, self._end = value, self._start
+        else:
+            self._end = value
+            
+    @property
+    def range(self) -> tuple[C | None, C | None]:
+        return self._start, self._end
+    
+    @range.setter
+    def range(self, value: tuple[C | None, C | None]):
+        start, end = value
+        if start is not None and end is not None and end > start:
+            self._start = end
+            self._end = start
+        else:
+            self._start = start
+            self._end = end
+    
+    @property
+    def column(self) -> str:
+        return self._col.value
+    
+    def perform_range_query(self, df: pd.DataFrame) -> pd.DataFrame:
+        start = self._start
+        end = self._end
+        
+        query_elements = []
+
+        if self._start is not None:
+            element = f"{self._col.value} >= @start"
+            query_elements.append(element)
+        if self._end is not None:
+            element = f"{self._col.value} < @end"
+            query_elements.append(element)
+        
+        if len(query_elements) == 0:
+            return df
+        elif len(query_elements) == 1:
+            query = query_elements[0]
+        else:
+            query = ' & '.join(query_elements)
+        return df.query(query)
+
+
+@dataclass
+class Query:
+    psd: QueryRange = field(
+        default_factory=lambda: QueryRange[float](PsdDfColumn.PSD))
+    energy: QueryRange = field(
+        default_factory=lambda: QueryRange[float](PsdDfColumn.ENERGY))
+    time: QueryRange = field(
+        default_factory=lambda: QueryRange[datetime](PsdDfColumn.EVENT_TIME))
+    
+    def perform_query(self, df: pd.DataFrame) -> pd.DataFrame:
+        query_result = df
+        query_fields = fields(self)
+        for query_field in query_fields:
+            query_range: QueryRange = getattr(self, query_field.name)
+            query_result = query_range.perform_range_query(query_result)
+        return query_result
+
+
+class SignalInvestigator:
+    def __init__(self, neutron_events_df: pd.DataFrame):
+        self._neutron_event_df = neutron_events_df
+        self._neutron_signals_df = generate_neutron_signals(neutron_events_df)
+        self.query = Query()
+        
+    def perform_query(self) -> pd.DataFrame:
+        return self.query.perform_query(self._neutron_event_df)
+    
+    def visualize_query(self) -> tuple[Figure, Axes]:
+        query_result = self.perform_query()
+        series_names = []
+        
+        fig, ax = plt.subplots(figsize=(FIG_DIM_X, FIG_DIM_Y))
+        
+        for index, row in query_result.iterrows():
+            n_psd = row[PsdDfColumn.PSD.value]
+            n_eng = row[PsdDfColumn.ENERGY.value]
+            n_time = row[PsdDfColumn.EVENT_TIME.value]
+            n_ps_remain = row[PsdDfColumn.EVENT_TIME_PS.value]
+            
+            series_name = (f"Time {n_time} +{n_ps_remain}ps, ",
+                           f"PSD {n_psd:.4f}, E {n_eng:.4f} MeVee")
+            series_names.append(series_name)
+            ax.plot(0-self._neutron_signals_df[index])  # type: ignore
+        
+        x_start, x_end = ax.get_xlim()
+        ax.xaxis.set_ticks(arange(x_start, x_end, 10))  # type: ignore
+        ax.legend(series_names)
+        
+        return fig, ax
