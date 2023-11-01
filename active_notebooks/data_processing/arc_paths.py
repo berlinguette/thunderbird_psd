dbec8eef86abef3016fe1ac07254d6d6b632776f
Ryan Oldford <ryan.oldford@gmail.com>
2023-11-01 11:28:54 -0700

    New function to get experiment root folder

--- active_notebooks/data_processing/arc_paths.py
+++ active_notebooks/data_processing/arc_paths.py
@@ -7,9 +7,11 @@ maybe_output = config.get("OUTPUT_DATA_FOLDER")
 INPUT_DATA_FOLDER = Path(maybe_input if maybe_input is not None else "fix_input")
 OUTPUT_DATA_FOLDER = Path(maybe_output if maybe_output is not None else "fix_output")
 
+def get_exp_root(experiment_name: str) -> Path:
+    return INPUT_DATA_FOLDER / experiment_name
+
 def get_parq_root(experiment_name: str) -> Path:
-    return INPUT_DATA_FOLDER.joinpath(experiment_name, 
-                                      'processed_data/unfiltered/psd')
+    return get_exp_root(experiment_name) / 'processed_data/unfiltered/psd'
 
 def get_report_root(experiment_name: str) -> Path:
     report_root = OUTPUT_DATA_FOLDER / experiment_name
@@ -17,5 +19,4 @@ def get_report_root(experiment_name: str) -> Path:
     return report_root
 
 def get_signals_root(experiment_name: str) -> Path:
-    return INPUT_DATA_FOLDER.joinpath(experiment_name,
-                                      'processed_data/unfiltered/signals')
+    return get_exp_root(experiment_name) / 'processed_data/unfiltered/signals'
