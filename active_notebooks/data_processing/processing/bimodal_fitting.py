bf3edfd264b36bf5d226aa7552baf7eda9710ae0
Ryan Oldford <ryan.oldford@gmail.com>
2023-11-02 12:32:45 -0700

    bugfixing

--- active_notebooks/data_processing/processing/bimodal_fitting.py
+++ active_notebooks/data_processing/processing/bimodal_fitting.py
@@ -163,7 +163,7 @@ def scan_histogram_slices(
         SliceFitter(bins, default_bounds, bounds), 
         enumerate(energy_slices), 
         chunksize=chunksize)
-    print([result for result in results])
+    print([len(result) if result is None else "None" for result in results])
     if any([result is None for result in results]):
         return None
     slice_params, slice_err = zip(*results)
