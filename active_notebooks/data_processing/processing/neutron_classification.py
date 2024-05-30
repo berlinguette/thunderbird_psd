4fad7e46c07aeb43cdfbaf4ae5cd4cc4286e8ec0
Ryan Oldford <ryan.oldford@gmail.com>
2024-05-29 20:10:06 -0700

    New classification window added

--- active_notebooks/data_processing/processing/neutron_classification.py
+++ active_notebooks/data_processing/processing/neutron_classification.py
@@ -8,6 +8,7 @@ from data_processing.processing.processing_configs import \
     DEFAULT_LOWER_ENERGY_BOUND
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
+from scipy.optimize import root_scalar
 from data_processing.dataframe_validation import get_df_col
 
 
@@ -44,6 +45,84 @@ def generate_nasa_neutron_window(
         return neutron_lb_fit(x) + window_offset
     
     return WindowBorders(left=lower_energy_bound, bottom=neutron_lb_fit, top=neutron_ub_fit, right=None)
+
+
+def generate_new_neutron_window(
+    slice_fit_df: pd.DataFrame,
+    energy_bin_edges: np.ndarray,
+    sigma: float = 3,
+    fom_energy_guess: float = 0.2
+) -> WindowBorders:
+    energy_bin_left_edges = energy_bin_edges[:-1]
+    energy_bin_right_edges = energy_bin_edges[1:]
+    energy_bin_midpoints = np.ndarray([
+        (lb+ub)/2 for lb, ub 
+        in zip(energy_bin_left_edges, energy_bin_right_edges)
+    ])
+    left_border = _generate_new_window_left_border(
+        slice_fit_df, energy_bin_midpoints, fom_energy_guess=fom_energy_guess
+    )
+    right_border = 688  # keVee, Compton edge + detector resolution
+    bottom_border = _generate_new_window_bottom_border(slice_fit_df, energy_bin_left_edges, sigma)
+    top_border = _generate_new_window_top_border(slice_fit_df, energy_bin_left_edges, sigma)
+    return WindowBorders(
+        left=left_border, 
+        right=right_border, 
+        bottom=bottom_border, 
+        top=top_border
+    )
+
+
+def _generate_new_window_left_border(
+    slice_fit_df: pd.DataFrame, 
+    energy_bin_midpoints: np.ndarray, 
+    fom_energy_guess: float = 200
+) -> float:
+    # create interp: x = energy_bin_midpoints, y = fom
+    energy_fom_curve = interp1d(energy_bin_midpoints, slice_fit_df['fom'])
+    # modify function: lambda x: fn(x) - 1.27
+    curve_for_root_finding = lambda x: energy_fom_curve(x) - 1.27
+    # use root_scalar to find E where FOM = 1.27 and return
+    root_results = root_scalar(curve_for_root_finding, x0=fom_energy_guess)
+    if root_results.converged:
+        return root_results.root
+    else:
+        raise ValueError(
+            f"Left border could not be found after {root_results.iterations}: "+
+            f"{root_results.flag}"
+        )
+
+
+def _generate_new_window_bottom_border(
+    slice_fit_df: pd.DataFrame, 
+    energy_bin_left_edges: np.ndarray, 
+    sigma: float
+) -> WindowBorderFunction:
+    border_psd_values = slice_fit_df['mu1'] - sigma * slice_fit_df['sigma1']
+    border: WindowBorderFunction = interp1d(
+        energy_bin_left_edges,
+        border_psd_values,
+        kind='previous',
+        fill_value=(border_psd_values[0], border_psd_values[-1]),  # type: ignore
+        bounds_error=False
+    )
+    return border
+
+
+def _generate_new_window_top_border(
+    slice_fit_df: pd.DataFrame,
+    energy_bin_left_edges: np.ndarray, 
+    sigma: float
+) -> WindowBorderFunction:
+    border_psd_values = slice_fit_df['mu1'] + sigma * slice_fit_df['sigma1']
+    border: WindowBorderFunction = interp1d(
+        energy_bin_left_edges,
+        border_psd_values,
+        kind='previous',
+        fill_value=(border_psd_values[0], border_psd_values[-1]),  # type: ignore
+        bounds_error=False
+    )
+    return border
 
 
 def classify(
