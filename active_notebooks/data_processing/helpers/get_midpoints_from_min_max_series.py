3fface1d87a251d313d93079933d1b1973081eb1
Ryan Oldford <ryan.oldford@gmail.com>
2024-07-03 15:50:23 -0700

    Cleanup

--- active_notebooks/data_processing/helpers/get_midpoints_from_min_max_series.py
+++ active_notebooks/data_processing/helpers/get_midpoints_from_min_max_series.py
@@ -1,10 +1,13 @@
-from pandas import Series, Index
+from pandas import Index, Series
 from pandas.arrays import IntervalArray
 
-def get_midpoints_from_min_max_series(min_series: Series, max_series: Series, index: Index) -> Series:
+
+def get_midpoints_from_min_max_series(
+    min_series: Series, max_series: Series, index: Index
+) -> Series:
     intervals = Series(
         IntervalArray.from_arrays(min_series, max_series),
         index=index,
     )
-    midpoints = Series(intervals.array.mid, index=index)
+    midpoints = Series(intervals.array.mid, index=index)  # type: ignore
     return midpoints
