1cc53e5fa6a2b8003078dff1416506d482fad83e
Ryan Oldford <ryan.oldford@gmail.com>
2024-07-16 11:06:06 -0700

    Cleanup

--- active_notebooks/data_processing/processing/slice_fitting/bimodal_fitting.py
+++ active_notebooks/data_processing/processing/slice_fitting/bimodal_fitting.py
@@ -1,5 +1,5 @@
-from typing import Any
 from math import ceil
+from typing import Any
 
 import numpy as np
 from data_processing.helpers.get_midpoints_from_bins import get_midpoints_from_bins
@@ -24,34 +24,35 @@ def get_bimodal_fit_guess(
     slice_samples = histogram_slice.shape[0]
     peak_search_settings: dict[str, Any] = dict(
         prominence=max(max(histogram_slice) / 10, 1),
-        wlen=ceil(slice_samples/2),
-        distance=slice_samples/4,
+        wlen=ceil(slice_samples / 2),
+        distance=slice_samples / 4,
     )
     peaks, properties = find_peaks(histogram_slice, **peak_search_settings)
-    
+
     if len(peaks) > 2:
         # pick out 2 peaks with lowest index
         indexes = np.argsort(peaks)
         peaks = peaks[indexes[:2]]
-        
+
         new_properties = {}
         for k, v in properties.items():
             new_v = v[indexes[:2]]
             new_properties[k] = new_v
         properties = new_properties
-    
+
     prominences = properties.get("prominences")
     left_bases = properties.get("left_bases")
     right_bases = properties.get("right_bases")
     prom_data_is_none = any([x is None for x in [prominences, left_bases, right_bases]])
-    prominence_data = None if prom_data_is_none else (prominences, left_bases, right_bases)
+    prominence_data = (
+        None if prom_data_is_none else (prominences, left_bases, right_bases)
+    )
     peak_width_settings: dict[str, Any] = dict(
         rel_height=0.5,
         prominence_data=prominence_data,
     )
     widths, *_ = peak_widths(histogram_slice, peaks, **peak_width_settings)
-    
-    # TODO get bimodal params for each peak
+
     if len(peaks) == 0:
         A_left = 0
         mu_left = 0
@@ -62,7 +63,7 @@ def get_bimodal_fit_guess(
         A_left = histogram_slice[left_peak_idx]
         mu_left = bin_mids[left_peak_idx]
         sigma_left = sample_psd_rate * (left_peak_width / 2)
-    
+
     if len(peaks) < 2:
         A_right = 0
         mu_right = 0
@@ -111,11 +112,9 @@ def get_bimodal_fit(
         lo_bounds, hi_bounds = bounds
         unpacked_lo = unpack_bimodal_params(lo_bounds)
         unpacked_hi = unpack_bimodal_params(hi_bounds)
-        curve_fit_params['bounds'] = (unpacked_lo, unpacked_hi)
+        curve_fit_params["bounds"] = (unpacked_lo, unpacked_hi)
 
-    params, cov = curve_fit(
-        bimodal, bin_midpoints, histogram_slice, **curve_fit_params
-    )
+    params, cov = curve_fit(bimodal, bin_midpoints, histogram_slice, **curve_fit_params)
 
     params = BimodalParams(*params)
     gamma_params, neutron_params = split_params(params)
