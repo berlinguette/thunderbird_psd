bf3b5472a907d14916f359a551f87b32cac1470c
Ryan Oldford <ryan.oldford@gmail.com>
2024-08-16 15:57:19 -0700

    Typing fix

--- active_notebooks/data_processing/helpers/get_input_required.py
+++ active_notebooks/data_processing/helpers/get_input_required.py
@@ -6,9 +6,7 @@ T = TypeVar('T')
 def get_input_required(
     prompt: str, allowable_values: list[T], converter: Callable[[str], T]
 ) -> T:
-    done = False
-    value = None
-    while not done:
+    while True:
         raw_value = input(prompt)
         try:
             value = converter(raw_value)
@@ -18,5 +16,4 @@ def get_input_required(
         if value not in allowable_values:
             print("The given value is not one of the allowable options.  Please try again.")
             continue
-        done = True
-    return value
+        return value
