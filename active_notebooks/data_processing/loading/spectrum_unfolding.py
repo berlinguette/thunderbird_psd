3747880c376f8545ecff7d7ae9e2d591e5452ef9
Ryan Oldford <ryan.oldford@gmail.com>
2024-12-16 14:20:08 -0800

    Can now load neutron response matrix

---
+++ active_notebooks/data_processing/loading/spectrum_unfolding.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from data_processing.processing.spectrum_unfolding import Histogram2D
+
+
+def load_neutron_response_matrix(
+    dir: Path, min_L: float = 0, max_L: float = 7, L_bin_widths: float = 0.005
+) -> Histogram2D:
+    widths = [15, 15, 20, 15, 15, 15, 15, 15]
+    output_files = [file for file in dir.iterdir() if "output" in file.stem]
+    if len(output_files) <= 0:
+        raise ValueError("Provided folder contains no 'output' data files")
+    response_sim_data: list[tuple[np.float64, np.ndarray]] = []
+    response_Ls: np.ndarray | None = None
+
+    for output_file in output_files:
+        df = pd.read_fwf(output_file, widths=widths)
+
+        source_e = df["source_e (MeV)"][0].astype(np.float64)
+        assert all(df["source_e (MeV)"] == source_e)
+
+        bins = np.arange(min_L, max_L + L_bin_widths, L_bin_widths)
+        cut = pd.cut(df["det_pulse (MeVee)"], bins.tolist())
+        cut_index = cut.cat.categories
+        new_df = pd.DataFrame(
+            df["NPS"].groupby(cut, observed=True).sum().reindex(cut_index, fill_value=0)
+        )
+
+        old_index = new_df.index
+        if not isinstance(old_index, pd.IntervalIndex):
+            raise RuntimeError(
+                f"DataFrame created from cut/groupby for {output_file.name} "
+                + "was not an IntervalIndex as expected"
+            )
+        mids = old_index.mid.to_series(index=cut_index)
+
+        np_cps = new_df["NPS"].to_numpy()
+        np_Ls = mids.to_numpy()
+        print(np_Ls)
+
+        response_sim_data.append((source_e, np_cps))
+        if response_Ls is None:
+            response_Ls = np_Ls
+
+    response_sim_data = sorted(response_sim_data, key=lambda x: float(x[0]))
+    response_Es, response_histos = zip(*response_sim_data)
+
+    np_R = np.array(response_histos).T
+    np_Es = np.array(response_Es)
+    return Histogram2D(np_R, np_Ls, np_Es)
---
+++ active_notebooks/data_processing/tests/loading/test_spectrum_unfolding.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from unittest.mock import PropertyMock
+
+import data_processing.loading.spectrum_unfolding as spec_unfold
+import numpy as np
+import pandas as pd
+import pytest
+from data_processing.loading.spectrum_unfolding import load_neutron_response_matrix
+from pyfakefs.fake_filesystem import FakeFilesystem
+
+test_dir = "test_dir"
+
+
+def test_good_path(fs: FakeFilesystem):
+    contents1 = """
+ source_e (MeV)            NPS   det_pulse (MeVee)       det_cell     corr_count            PSD       time (s)  cells_history
+
+
+              1              9           0.0546242              1             no       0.290346    9.57902e-09          4  1 
+              1             21            0.099207              1             no       0.287368    1.10722e-08          4  1 
+              1             37           0.0627974              1             no       0.341804    9.62823e-09          4  1 
+              1             44           0.0310387              1             no       0.243887    1.01032e-08          4  1 
+              1             64           0.0647629              1             no       0.341947    9.20444e-09          4  1 
+"""
+    contents2 = """
+ source_e (MeV)            NPS   det_pulse (MeVee)       det_cell     corr_count            PSD       time (s)  cells_history
+
+
+              2              9            0.124175              1             no       0.327161    6.83285e-09          4  1 
+              2             21            0.320092              1             no       0.389883      8.599e-09          4  1 
+              2             37            0.221753              1             no       0.387224    7.06528e-09          4  1 
+              2             44           0.0291878              1             no       0.194259    7.40726e-09          4  1 
+              2             64            0.217934              1             no       0.388198      6.594e-09          4  1 
+"""
+    fs.create_file("test_dir/output_1.txt", contents=contents1)
+    fs.create_file("test_dir/output_2.txt", contents=contents2)
+    R = load_neutron_response_matrix(Path(test_dir))
+    assert R.counts.shape == (1400, 2)
+    assert all([x == y for x, y in zip(list(R.y_midpoints), [1, 2])])
+    E_sums = R.counts.sum(axis=0)
+    assert all(E_sums == (9 + 21 + 37 + 44 + 64))
+
+
+def test_no_files(fs: FakeFilesystem):
+    fs.create_dir("test_dir")
+    with pytest.raises(ValueError) as excinfo:
+        load_neutron_response_matrix(Path(test_dir))
+    assert "data files" in str(excinfo.value)
+
+
+def test_bad_index_type(fs: FakeFilesystem, mocker):
+    contents1 = """
+ source_e (MeV)            NPS   det_pulse (MeVee)       det_cell     corr_count            PSD       time (s)  cells_history
+
+
+              1              9           0.0546242              1             no       0.290346    9.57902e-09          4  1 
+              1             21            0.099207              1             no       0.287368    1.10722e-08          4  1 
+              1             37           0.0627974              1             no       0.341804    9.62823e-09          4  1 
+              1             44           0.0310387              1             no       0.243887    1.01032e-08          4  1 
+              1             64           0.0647629              1             no       0.341947    9.20444e-09          4  1 
+"""
+    contents2 = """
+ source_e (MeV)            NPS   det_pulse (MeVee)       det_cell     corr_count            PSD       time (s)  cells_history
+
+
+              2              9            0.124175              1             no       0.327161    6.83285e-09          4  1 
+              2             21            0.320092              1             no       0.389883      8.599e-09          4  1 
+              2             37            0.221753              1             no       0.387224    7.06528e-09          4  1 
+              2             44           0.0291878              1             no       0.194259    7.40726e-09          4  1 
+              2             64            0.217934              1             no       0.388198      6.594e-09          4  1 
+"""
+    fs.create_file("test_dir/output_1.txt", contents=contents1)
+    fs.create_file("test_dir/output_2.txt", contents=contents2)
+    mock_index = mocker.patch.object(
+        spec_unfold.pd.DataFrame, "index", new_callable=PropertyMock
+    )
+    mock_index.return_value = None
+    with pytest.raises(RuntimeError):
+        load_neutron_response_matrix(Path(test_dir))
